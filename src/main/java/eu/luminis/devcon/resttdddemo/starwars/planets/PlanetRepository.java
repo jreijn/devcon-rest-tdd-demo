c6deefc51c3a7a269b08bb0c9e50040cb33a7dc7
Jeroen Reijn <j.reijn@gmail.com>
2018-03-28 11:37:11 +0200

    Remove items

--- src/main/java/eu/luminis/devcon/resttdddemo/starwars/planets/PlanetRepository.java
+++
@@ -1,9 +0,0 @@
-package eu.luminis.devcon.resttdddemo.starwars.planets;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlanetRepository extends JpaRepository<Planet,Long> {
-
-}
